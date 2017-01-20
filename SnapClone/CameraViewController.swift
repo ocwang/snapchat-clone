ad7f28737e1a0e7e5feedf037f545f1583368df6
Jake Zeal <jake@jakezeal.com>
2017-01-20 15:12:11 -0800

    initial preview layer setup

--- SnapClone/CameraViewController.swift
+++ SnapClone/CameraViewController.swift
@@ -7,12 +7,71 @@
 //
 
 import UIKit
+import FirebaseAuth
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
+    // MARK: - Instance Vars
+    let captureSession = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    let imageOutput = AVCapturePhotoOutput()
+    let movieOutput = AVCaptureMovieFileOutput()
+    
+    // MARK: - Subviews
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var cameraView: UIView!
+    
+    // MARK: - View Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        setupAudioInput()
+//        setupImageOutput()
+//        setupMovieOutput()
+        configureCameraInput()
+        setupPreviewLayer()
+        captureSession.startRunning()
     }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    func configureCameraInput(with position: AVCaptureDevicePosition = .back) {
+        let cameraDevice = cameraWithPosition(position: position)
+        
+        do {
+            let newCameraInput = try AVCaptureDeviceInput(device: cameraDevice)
+            
+            if captureSession.canAddInput(newCameraInput) {
+                captureSession.addInput(newCameraInput)
+            }
+        } catch let error as NSError {
+            assertionFailure("Error: \(error.localizedDescription)")
+        }
+        
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+    }
+    
+    func setupPreviewLayer() {
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        previewLayer.frame = cameraView.bounds
+        cameraView.layer.addSublayer(previewLayer)
+    }
+    
+    // MARK: - IBActions
+    @IBAction func recordButtonTapped(_ sender: Any) {
+    
+    }
+    
+    func cameraWithPosition(position: AVCaptureDevicePosition) -> AVCaptureDevice? {
+        return AVCaptureDevice.defaultDevice(withDeviceType: AVCaptureDeviceType.builtInWideAngleCamera,
+                                             mediaType: AVMediaTypeVideo,
+                                             position: position)
+    }
+
 }
 
